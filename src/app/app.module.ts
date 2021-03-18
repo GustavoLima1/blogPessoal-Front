9cbc02159efec52d4767aa171e2ee400fc646cf1
Gustavo Lima <gustavo1.l@hotmail.com>
2021-03-17 21:05:43 -0300

    Implementado componentes entrar e cadastrar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
 import { RodapeComponent } from './rodape/rodape.component';
+import { LoginComponent } from './login/login.component';
+import { CadastrarComponent } from './cadastrar/cadastrar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
-    RodapeComponent
+    RodapeComponent,
+    LoginComponent,
+    CadastrarComponent
   ],
   imports: [
     BrowserModule
